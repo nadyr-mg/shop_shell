08d406d03d8b9c214de93f99abbfbdab046a3979
marshygeek <lester0578@gmail.com>
2017-10-25 23:08:11 +0400

    Trying to activate webhook on the new domain

--- application.py
+++ application.py
@@ -11,7 +11,7 @@ bot = get_bot()
 @application.route('/{}'.format(config.TOKEN), methods=['POST'])
 def parse_request():
     bot.process_new_updates([types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return 'OK', 200
+    return '', 200
 
 
 @application.route('/')
