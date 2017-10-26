44ff41ba9809341ec436516d58ca9f5985ccd6fb
marshygeek <lester0578@gmail.com>
2017-10-26 11:43:59 +0400

    d

--- application.py
+++ application.py
@@ -38,6 +38,6 @@ def start_command(message):
 if __name__ == "__main__":
     bot.remove_webhook()
     sleep(1)
-    bot.set_webhook(url="https://{}/{}/".format(config.EBCLI_DOMAIN, config.TOKEN))
+    bot.set_webhook(url="https://{}/{}".format(config.EBCLI_DOMAIN, config.TOKEN))
 
     application.run(host=config.WEBHOOK_LISTEN, port=config.WEBHOOK_PORT)
